d58a38ecb8119c7897225bf1f24cb9dce4b022ff
Adrian Weerasinghe <adrian97.thilina@gmail.com>
2024-05-27 05:32:13 +1000

    code updated

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+// eslint.config.js
+export default [
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "single"],
+    },
+  },
+];
