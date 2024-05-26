84ef36555429279fceaf8f9fd7b720d1786b1d6f
Adrian Weerasinghe <adrian97.thilina@gmail.com>
2024-05-27 06:30:49 +1000

    code updated

---
+++ test/java/com/example/YourTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class YourTest {
+    @Test
+    public void testSomething() {
+        assertTrue(true);
+    }
+}
